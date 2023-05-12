136479a5962485ce679e67801caf388ee4088b62
Bright Mensah <brightmensah064@gmail.com>
2023-05-12 17:06:46 +0000

    we worked the final part of 3.3

--- index.js
+++ index.js
@@ -37,10 +37,25 @@ console.log(driverName);
 ///
 // another option
 // split(), reverse(), join(),
-
+//code for 3.2
 let reverseName = " ";
 for (let i = hacker1.length - 1; i >= 0; i--) {
-  reverseName = hacker1.toUpperCase();
-  //   reverseName += hacker1[i].toUpperCase() + " ";
+  reverseName += hacker1[i].toUpperCase() + " ";
 }
 console.log(reverseName);
+
+// code for 3.3
+for (let i = 0; i < hacker1.length && i < hacker2.length; i++) {
+  if (hacker1[i] < hacker2[i]) {
+    console.log("The driver's name goes first.");
+    break;
+  } else if (hacker1[i] > hacker2[i]) {
+    console.log("Yo, the navigator goes first, definitely.");
+    break;
+  } else {
+    continue;
+  }
+}
+if (hacker1.length === hacker2.length) {
+  console.log("What?! You both have the same name?.");
+}
